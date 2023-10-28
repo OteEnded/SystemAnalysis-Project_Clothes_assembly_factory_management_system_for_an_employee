0485eb68c6d69c9659ee9338b0a6963bcba2db19
Wr3nch-ren <prayhyper@gmail.com>
2023-10-28 17:26:07 +0700

    Added seed() method, changed tables to final (because it's never getting queried, for now.). [Maybe it works IDK man].

--- src/main/java/ku/cs/service/DBSeeder.java
+++ src/main/java/ku/cs/service/DBSeeder.java
@@ -1,13 +1,17 @@
 package ku.cs.service;
 
 import ku.cs.entity.*;
+import ku.cs.model.Material;
+import ku.cs.model.Product;
 import ku.cs.model.SQLTable;
 
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 public class DBSeeder {
     // list out tables that need to be seeded here
-    private static ArrayList<SQLTable> tables = new ArrayList<>();
+    private static final ArrayList<SQLTable> tables = new ArrayList<>();
     static {
         tables.add(Users.getSqlTable());
         tables.add(Materials.getSqlTable());
@@ -17,18 +21,32 @@ public class DBSeeder {
         tables.add(DailyRecords.getSqlTable());
     }
 
-    public static void seed() {
-        // seed data here
-//        try {
-//            Users.load();
-//            Materials.load();
-//            Products.load();
-//            MaterialUsages.load();
-//            Works.load();
-//            DailyRecords.load();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        // not sure if this works
+    public static void seed() throws SQLException, ParseException {
+        Product p1 = new Product();
+        p1.setName("pant");
+        p1.setSize(15);
+        p1.setProgressRate(5.00);
+        p1.save();
+
+        Product p2 = new Product();
+        p2.setName("shirt");
+        p2.setSize(10);
+        p2.setProgressRate(5.00);
+        p2.save();
+
+        Material m1 = new Material();
+        m1.setName("cloth");
+        m1.setUnit("meter");
+        m1.save();
+
+        Material m2 = new Material();
+        m2.setName("button");
+        m2.setUnit("piece");
+        m2.save();
+
+        Material m3 = new Material();
+        m3.setName("thread");
+        m3.setUnit("meter");
+        m3.save();
     }
 }

b2d42f124f0de0ee52360a588c2c326af3699ec1
xMickeyS <pichaya.sen@ku.th>
2023-10-28 15:49:59 +0700

    update wait-for-receive page

--- src/main/java/ku/cs/controller/employer/work/WaitForReceivePageController.java
+++ src/main/java/ku/cs/controller/employer/work/WaitForReceivePageController.java
@@ -1,10 +1,59 @@
 package ku.cs.controller.employer.work;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import ku.cs.controller.Work;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class WaitForReceivePageController {
+
+    @FXML private TableView<Work> tableView;
+    @FXML private TableColumn<Work, String> type;
+    @FXML private TableColumn<Work, String> product;
+    @FXML private TableColumn<Work, Integer> quantity;
+    @FXML private TableColumn<Work, LocalDate> deadline;
+
+    // work detail
+    @FXML private Label workTypeLabel;
+    @FXML private Label productLabel;
+    @FXML private Label deadlineLabel;
+    @FXML private Label amountLabel;
+
+//    @FXML
+//    void initialize() {
+//        // Phubest Code ()
+//        type.setCellValueFactory(new PropertyValueFactory<>("type"));
+//        product.setCellValueFactory(new PropertyValueFactory<>("product"));
+//        quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+//        deadline.setCellValueFactory(new PropertyValueFactory<>("deadline"));
+//
+//        ObservableList<Work> works = FXCollections.observableArrayList();
+//        works.add(new Work("งานธรรมดา", "กระโปรง ขนาด 20 นิ้ว", 20, LocalDate.now(), "ทันตามกำหนด", 10, "note"));
+//        works.add(new Work("งานธรรมดา", "กระโปรง ขนาด 20 นิ้ว", 20, LocalDate.now(), "ทันตามกำหนด", 10, "note"));
+//        tableView.setItems(works);
+//        handleSelectedRow();
+//    }
+//
+//    private void handleSelectedRow() {
+//        tableView.getSelectionModel().selectedItemProperty().addListener(
+//                (observable, oldValue, newValue) -> {
+//                    showSelectedRow(newValue);
+//                }
+//        );
+//    }
+//
+//    private void showSelectedRow(Work newValue) {
+//        // productLabel.setText(newValue.getProduct());
+//        productLabel.setText(newValue.toString());
+//    }
+
 
     // MenuBar Handle
     @FXML
